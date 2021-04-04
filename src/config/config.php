bc9a29f3eb6b8e05f9c75464fc45840779b46aed
Sergio Felzener <felzenerdj@gmail.com>
2021-04-04 15:55:14 -0300

    start login

---
+++ src/config/config.php
@@ -0,0 +1,35 @@
+<?php 
+
+// definindo timezone da aplicação
+date_default_timezone_set('America/Sao_Paulo');
+// ajustando a parte específica de tempo passando 4 parametros para portugues Brasil
+setlocale(LC_TIME, 'pt-BR', 'pt-BR.uft-8', 'portuguese');
+
+// constantes gerais
+define('DAILY_TIME', 60 * 60 * 8);
+
+
+
+//Pastas do projeto
+
+// definindo constante com define vai pegar o path de dentro do dir models usa /../ para sair da pasta config 
+define('MODEL_PATH', realpath(dirname(__FILE__) . '/../models'));
+define('VIEW_PATH', realpath(dirname(__FILE__) . '/../views'));
+define('TEMPLATE_PATH', realpath(dirname(__FILE__) . '/../views/template'));
+define('CONTROLLER_PATH', realpath(dirname(__FILE__) . '/../controllers'));
+define('EXCEPTION_PATH', realpath(dirname(__FILE__) . '/../exceptions'));
+
+//carregando arquivo da pasta config.
+require_once(realpath(dirname(__FILE__) . '/database.php'));
+// require_once(realpath(dirname(__FILE__) . '/loader.php'));
+// require_once(realpath(dirname(__FILE__) . '/session.php'));
+// require_once(realpath(dirname(__FILE__) . '/date_utils.php'));
+// require_once(realpath(dirname(__FILE__) . '/utils.php'));
+require_once(realpath(MODEL_PATH . '/Model.php'));
+require_once(realpath(MODEL_PATH . '/User.php'));
+// require_once(realpath(MODEL_PATH . '/WorkingHours.php'));
+// require_once(realpath(EXCEPTION_PATH . '/AppException.php'));
+// require_once(realpath(EXCEPTION_PATH . '/ValidationException.php'));
+
+
+?>
---
+++ src/views/login.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="assets/css/comum.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/icofont.min.css">
+    <link rel="stylesheet" href="assets/css/login.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <title>Record - Time to Clock In</title>
+</head>
+
+<body class="bg-white">
+
+    <form class="form-login" action="#" method="POST">
+        <div class="login-card card shadow-lg border-1 border-gray-400">
+
+            <div class="card-header bg-blue-400">
+                <img class="w-80 mx-auto mt-0" src="../assets/img/logopreto.png">
+            </div>
+            <div class="flex justify-center content-center px-3 pt-2">
+                <h2 class="text-xl font-black">LOGIN</h2>
+            </div>
+            <div class="card-body">
+                <?php include(TEMPLATE_PATH . '/messages.php') ?>
+                <div class="form-group">
+                    <label for="email">E-Mail</label> <!-- Passando o caminho correto caso nao passe o $_POST no metodo loadView('name da view', $_POST['email']) para manter o valor do email salvo na tela com o Value-->
+                    <input type="email" id="email" name="email" class="form-control <?= $errors['email'] ? 'is-invalid' : '' ?>" value="<?= $email ?> " placeholder="Informe o e-mail" autofocus>
+                    <div class="invalid-feedback"><?= $errors['email'] ?></div>
+                </div>
+                <div class="form-group">
+                    <label for="password">Senha</label>
+                    <input type="password" id="password" name="password" class="form-control <?= $errors['password'] ? 'is-invalid' : '' ?>" placeholder="Informe a senha" autofocus>
+                    <div class="invalid-feedback"><?= $errors['password'] ?></div>
+                </div>
+            </div>
+            <div class="card-footer bg-black">
+             
+                    <span class="text-gray-200">Sistema de ®egistro de ponto.</span>
+                
+                <button class="btn btn-primary float-right px-6">Entrar</button>
+            </div>
+        </div>
+    </form>
+    <?= $texto ?>
+</body>
+
+</html>

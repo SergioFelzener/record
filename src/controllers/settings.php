2452cc156290235668330319961f73db781492d5
Sergio Felzener <felzenerdj@gmail.com>
2021-05-09 18:43:55 -0300

    exibindo notificacoes em settings

--- src/controllers/settings.php
+++ src/controllers/settings.php
@@ -4,6 +4,10 @@ session_start();
 requireValidSession(true);
 
 loadModel('Settings');
+$user = $_SESSION['user'];
+$selectedUserId = $user->id;
+$exception = null;
+$notActive = Notifications::getResultSetFromSelect(['user_id' => $selectedUserId, 'active' => 1], 'active');
 
 
-loadTemplateView('settings');
+loadTemplateView('settings', ['exception' => $exception, 'notActive' => $notActive]);

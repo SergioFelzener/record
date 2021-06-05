ea9f0f7164affcee877e3065a45b6b8c130d1f63
Sergio Felzener <felzenerdj@gmail.com>
2021-06-05 14:11:41 -0300

    colocando hotjar na pagina day_records

--- src/views/day_records.php
+++ src/views/day_records.php
@@ -1,6 +1,17 @@
 <head>
     <meta charset="UTF-8">
     <!-- <link rel="stylesheet" href="assets/css/bootstrap.min.css"> -->
+    <!-- Hotjar Tracking Code for http://52.152.225.175 -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:2438765,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
 </head>
 
 

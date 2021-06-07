b13706e8e9632d7045717454851de006a5e9eeeb
Sergio Felzener <felzenerdj@gmail.com>
2021-06-07 19:15:58 -0300

    finish

--- src/config/config.php
+++ src/config/config.php
@@ -4,7 +4,7 @@
 date_default_timezone_set('America/Sao_Paulo');
 
 // ajustando a parte específica de tempo passando 4 parametros para portugues Brasil
-setlocale(LC_ALL, 'pt_BR');
+// setlocale(LC_ALL, 'pt_BR');
 setlocale(LC_TIME, 'pt_BR', 'pt_BR.uft-8', 'portuguese');
 
 // constantes gerais
